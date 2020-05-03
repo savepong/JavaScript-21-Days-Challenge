821c40e1c0c9b2a4efe9f6a83bb63d8a6abf6df4
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2020-05-03 23:09:37 +0700

    implement countdown timer

--- 02 - Countdown Timer/start.js
+++ 02 - Countdown Timer/start.js
@@ -1,4 +1,31 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const SECOND = 1000;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+  const DAY = HOUR * 24;
 
+  function setElementInnerText(id, text) {
+    const element = document.getElementById(id);
+    element.innerText = text;
+  }
+
+  function countDown() {
+    const now = new Date().getTime();
+    const newYear = new Date("January 1, 2021 00:00:00").getTime();
+    const unixtimeLeft = newYear - now;
+
+    setElementInnerText("days", Math.floor(unixtimeLeft / DAY));
+    setElementInnerText("hours", Math.floor((unixtimeLeft % DAY) / HOUR));
+    setElementInnerText("minutes", Math.floor((unixtimeLeft % HOUR) / MINUTE));
+    setElementInnerText(
+      "seconds",
+      Math.floor((unixtimeLeft % MINUTE) / SECOND)
+    );
+  }
+
+  function run() {
+    setInterval(countDown, SECOND);
+  }
+
+  run();
 })();
