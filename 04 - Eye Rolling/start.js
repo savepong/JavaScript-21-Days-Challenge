740b1cf9e703f4598aa7a0e524136891bb14b3e2
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2020-05-03 23:49:19 +0700

    Implement eys rolling

--- 04 - Eye Rolling/start.js
+++ 04 - Eye Rolling/start.js
@@ -1,4 +1,24 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  function run() {
+    const bodyElem = document.querySelector("body");
+    const eyeElem = document.querySelectorAll(".eye");
+
+    function onMouseMove({ pageX, pageY }) {
+      eyeElem.forEach((eyeElem) => {
+        const { left, top } = eyeElem.getBoundingClientRect();
+
+        const eyeCenterX = left + eyeElem.offsetWidth / 2;
+        const eyeCenterY = left + eyeElem.offsetHeight / 2;
+        const radian = Math.atan2(pageX - eyeCenterX, pageY - eyeCenterY);
+        const angle = ((radian * 180) / Math.PI) * -1;
+        eyeElem.style.transform = `rotate(${angle}deg)`;
+      });
+    }
+
+    bodyElem.addEventListener("mousemove", onMouseMove);
+  }
+
+  run();
 })();
