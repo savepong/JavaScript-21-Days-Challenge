970022bf7e012bc90e746d46f11fea9dc741c07e
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2020-05-10 16:31:12 +0700

    + Weird Parts

--- 13 - JavaScript Weird Parts/start.js
+++ 13 - JavaScript Weird Parts/start.js
@@ -2,7 +2,36 @@
   // เริ่มเขียนโค้ด
 
   // 1. NaN
+  if (NaN * 1 === NaN) {
+    console.log("Equal");
+  }
+
+  const result = 1 / "hello";
+  if (Number.isNaN(result)) {
+    console.log("Equal to NaN");
+  }
+
   // 2. Type Coercion
+  if (3 > 2 > 1) {
+    console.log("Inside");
+  }
+
+  console.log(2 - "1");
+  console.log(2 + Number.parseInt("1", 10));
+
+  console.log(true + true);
+
   // 3. Interpreter & Compiler
+  function getPerson() {
+    return {
+      name: "Pongsiri",
+    };
+  }
+  console.log(getPerson());
+
   // 4. Checking Object Type
+  const person = {};
+  if (typeof person === "object" && person !== null) {
+    console.log("yes, object");
+  }
 })();
