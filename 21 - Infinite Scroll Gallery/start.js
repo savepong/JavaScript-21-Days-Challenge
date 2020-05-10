9f7f1d0431b06f9716d730094731b87b64729c89
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2020-05-10 19:08:08 +0700

    Implement Scroll Gallery

--- 21 - Infinite Scroll Gallery/start.js
+++ 21 - Infinite Scroll Gallery/start.js
@@ -1,3 +1,49 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const KEY = "j2fWiJaO4vE7v2YWFPnt5ft7gLjbNiWIS-8t-cePKQc";
+  const loaderElem = document.querySelector(".loader");
+  let page = 1;
+
+  function showLoader() {
+    loaderElem.classList.add("visible");
+  }
+
+  function hideLoader() {
+    loaderElem.classList.remove("visible");
+  }
+
+  async function displayImages() {
+    showLoader();
+
+    const result = await fetch(
+      `https://api.unsplash.com/photos/?client_id=${KEY}&page=${page}`
+    );
+    const images = await result.json();
+
+    const galleryElem = document.querySelector(".gallery");
+
+    images.forEach((image) => {
+      const imgElem = document.createElement("img");
+      imgElem.src = image.urls.small;
+      galleryElem.appendChild(imgElem);
+    });
+
+    hideLoader();
+
+    page += 1;
+  }
+
+  function onScroll() {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+      displayImages();
+    }
+  }
+
+  function run() {
+    document.addEventListener("scroll", onScroll);
+    displayImages();
+  }
+
+  run();
 })();
