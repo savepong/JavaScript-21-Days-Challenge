8bb36614cc6f8186513f94030add78bc15be1d1e
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2020-05-10 17:57:11 +0700

    Learn: What is "prototype"

--- 18 - What is prototype/start.js
+++ 18 - What is prototype/start.js
@@ -1,8 +1,31 @@
 (() => {
   // เริ่มเขียนโค้ด
 
-  // 1. Class vs Prototype 
+  // 1. Class vs Prototype
+  // class Person {}
+
+  // const pongsiri = new Person();
+  // console.log(pongsiri);
+
   // 2. What's prototype?
+  // const name = "Pongsiri";
+  // console.log(name);
+
+  // const arr = [];
+  // console.log(arr.__proto__);
+
   // 3. Prototype chain
+  // const name = "Pongsiri";
+  // console.log(name.__proto__);
+  // console.log(name.toLocaleString());
+
   // 4. Extend a prototype
+  const name = "Pongsiri";
+  function sayHello(val) {
+    console.log(`Hello ${val}`);
+  }
+
+  String.prototype.sayHello = sayHello;
+  console.log(name.__proto__);
+  name.sayHello("World");
 })();
